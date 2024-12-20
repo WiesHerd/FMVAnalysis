5b56f0aecb27305f9ba669a48dfd614aca96ecec
Wieslaw Herdzik <wherdzik@gmail.com>
2024-12-19 20:06:37 -0700

    Simplify Vercel and Vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,9 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server: {
-    port: 3000,
-    host: true
-  },
+  base: "/",
   build: {
     outDir: "dist",
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
+    sourcemap: true
+  }
 })
