fca68c9f075fc139fa8b13b96232ca8fc13029d6
Wieslaw Herdzik <wherdzik@gmail.com>
2024-12-19 20:23:08 -0700

    Update build configuration for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "./",
   build: {
     outDir: "dist",
     sourcemap: true,
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   server: {
     port: 3000,
