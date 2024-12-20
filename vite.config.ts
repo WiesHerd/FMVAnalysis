7cbc29fd311e8daed91b1edef019f33e971683cb
Wieslaw Herdzik <wherdzik@gmail.com>
2024-12-19 20:01:55 -0700

    Configure app for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,18 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
